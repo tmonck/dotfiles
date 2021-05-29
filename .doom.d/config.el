de2cd79f7b299cefbd156a1b684fcf7050891776
Tom Monck <tom.monck@dudesolutions.com>
2021-05-29 19:14:32 -0400

    Adjust theme

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -23,8 +23,8 @@
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+;; `load-theme' function. The is the default: doom-one
+(setq doom-theme 'doom-acario-dark)
 
 (if (eq initial-window-system 'x)                 ; if started by emacs command or desktop file
     (toggle-frame-maximized)
